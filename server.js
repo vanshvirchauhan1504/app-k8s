704388db9e93b11bb430ae3018384fd1d1c06f89
Vanshvir Chauhan <vanshvirchauhan1504@gmail.com>
2024-12-27 17:38:21 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: '*', // Allow requests from this origin
+  methods: 'GET', // Allow only GET and POST methods
+  allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
+};
+
+app.use(cors(corsOptions)); // Enable CORS for all routes
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello K8S SERVER!');
+});
+
+// Another route
+app.post('/data', (req, res) => {
+  // Handle data
+  res.json({ message: 'Data received' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
