8138d25aad59cb1353158563ade08f56d6cfa8d5
Vanshvir  Chauhan <97740365+vanshvirchauhan1504@users.noreply.github.com>
2024-12-30 16:59:27 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-console.log("HI TEST SCAN")
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'test_db'
+});
 
-cosnole.log(INTENTIONAL ERROR)
+const userId = req.query.userId; // Assume this comes from user input
+const query = `SELECT * FROM users WHERE id = ${userId}`; // Vulnerable to SQL Injection
+
+connection.query(query, (err, results) => {
+  if (err) throw err;
+  console.log(results);
+});
+
