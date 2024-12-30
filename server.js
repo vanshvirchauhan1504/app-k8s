ee5768b0c4930b9aaf4203861ca60476ccb694c8
Vanshvir  Chauhan <97740365+vanshvirchauhan1504@users.noreply.github.com>
2024-12-30 11:58:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const corsOptions = {
   origin: '*', // Allow requests from this origin
